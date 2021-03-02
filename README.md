d100768dfb8e3bcf1069e088e2d65e926f3c902d
张树涛 <jingchun@xiaohongshu.com>
2021-03-02 11:04:44 +0800

    搭建好vue框架并完成注册页面

---
+++ README.md
@@ -0,0 +1,25 @@
+# gin-test-vue
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+# gin-test-vue
---
+++ src/views/register/Register.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <b-row class="mt-5">
+      <b-col
+        md="8"
+        offset-md="2"
+        lg="6"
+        offset-lg="3"
+      >
+        <b-card title="注册">
+          <b-form>
+            <b-form-group label="姓名:">
+              <b-form-input
+                v-model="$v.user.name.$model"
+                type="text"
+                placeholder="输入您的名称（选填）"
+              ></b-form-input>
+            </b-form-group>
+            <b-form-group label="手机号:">
+              <b-form-input
+                v-model="$v.user.telephone.$model"
+                type="number"
+                placeholder="输入您的手机号 "
+                :state="validateState('telephone')"
+              ></b-form-input>
+              <b-form-invalid-feedback :state="validateState('telephone')">
+                手机号不符合要求
+              </b-form-invalid-feedback>
+            </b-form-group>
+            <b-form-group label="密码:">
+              <b-form-input
+                v-model="$v.user.password.$model"
+                type="password"
+                placeholder="输入您的密码 "
+                :state="validateState('password')"
+              ></b-form-input>
+              <b-form-invalid-feedback :state="validateState('password')">
+                密码必须大于等于 6 位
+              </b-form-invalid-feedback>
+            </b-form-group>
+            <b-form-group>
+              <b-button
+                @click="register"
+                variant="primary"
+                block
+              >注册</b-button>
+            </b-form-group>
+          </b-form>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import { required, minLength } from 'vuelidate/lib/validators';
+import consumValidator from '../../helper/validator';
+
+export default {
+  data() {
+    return {
+      user: {
+        name: '',
+        telephone: '',
+        password: '',
+      },
+    };
+  },
+  validations: {
+    user: {
+      name: {
+      },
+      telephone: {
+        required,
+        telephone: consumValidator.telephoneValidator,
+      },
+      password: {
+        required,
+        minLength: minLength(6),
+      },
+    },
+  },
+  methods: {
+    // 这里是es6 解构赋值
+    validateState(name) {
+      const { $dirty, $error } = this.$v.user[name];
+      return $dirty ? !$error : null;
+    },
+    register() {
+      // 验证数据
+      this.$v.user.$touch();
+      if (this.$v.user.$anyError) {
+        return;
+      }
+      // 请求
+      const api = 'http://localhost:8081/api/auth/register';
+      this.axios.post(api, { ...this.user }).then((res) => {
+        console.log(res.data);
+      }).catch((err) => {
+        console.log('err:', err.response.data.msg);
+      });
+      console.log('注册成功');
+    },
+  },
+};
+
+</script>
+<style scoped>
+</style>
